4a79134a1f8e2708d5d1e2be6c1efac2e117925b
Angela Du <narutorocks93@gmail.com>
2021-02-13 20:51:48 -0500

    worked on profile

--- src/Pages/Components/Profile.js
+++ src/Pages/Components/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 
+
 const Profile = () => {
 
   const { user, isAuthenticated } = useAuth0();
-  
+
     return (
         <div>
             {JSON.stringify(user, null, 2)}
